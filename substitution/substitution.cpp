4599eaaa801a45cb921b1774c7418401d867e84c
KRES-TR1 <125355882+KRES-TR1@users.noreply.github.com>
2023-09-21 10:58:43 +0700

    Update and rename substitution.c to substitution.cpp

--- substitution/substitution.cpp
+++ substitution/substitution.cpp
@@ -1,21 +1,19 @@
-#include <cs50.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
+#include <iostream>
+#include <cstring>
+#include <cctype>
 
-bool check_duli(string key);
+bool check_duli(std::string key);
 
-int main(int argc, string argv[])
+int main(int argc, char* argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./substitution key\n");
+        std::cout << "Usage: ./substitution key" << std::endl;
         return 1;
     }
-
     else if (strlen(argv[1]) == 26)
     {
-        string key = argv[1];
+        std::string key = argv[1];
         char input[26];
 
         for (int k = 0; k < strlen(argv[1]); k++)
@@ -23,22 +21,24 @@ int main(int argc, string argv[])
             input[k] = key[k];
             if (isdigit(input[k]))
             {
-                printf("Usage: ./substitution key\n");
+                std::cout << "Usage: ./substitution key" << std::endl;
                 return 0;
             }
             else if (!check_duli(argv[1]))
             {
-                printf("Key must contain 26 unique characters.\n");
+                std::cout << "Key must contain 26 unique characters." << std::endl;
                 return 1;
             }
-            string plaintext = get_string("Plaintext: ");
-            printf("ciphertext: ");
+            std::string plaintext;
+            std::cout << "Plaintext: ";
+            std::getline(std::cin, plaintext);
+            std::cout << "ciphertext: ";
             for (int j = 0; j < strlen(argv[1]); j++)
             {
                 input[j] = key[j];
             }
             int l = 0;
-            for (l = 0; l < strlen(plaintext); l++)
+            for (l = 0; l < plaintext.length(); l++)
             {
                 int index = 0;
 
@@ -46,39 +46,38 @@ int main(int argc, string argv[])
                 {
                     index = plaintext[l] - 65;
                     plaintext[l] = input[index];
-                    printf ("%c", toupper(plaintext[l]));
-
+                    std::cout << static_cast<char>(toupper(plaintext[l]));
                 }
                 else if (islower(plaintext[l]))
                 {
                     index = plaintext[l] - 97;
                     plaintext[l] = input[index];
-                    printf("%c", tolower(plaintext[l]));
+                    std::cout << static_cast<char>(tolower(plaintext[l]));
                 }
                 else
                 {
-                    printf("%c", plaintext[l]);
+                    std::cout << plaintext[l];
                 }
             }
-            printf("\n");
+            std::cout << std::endl;
             return 0;
         }
-
     }
     else if (strlen(argv[1]) < 26)
     {
-        printf("Key must contain 26 characters.\n");
+        std::cout << "Key must contain 26 characters." << std::endl;
         return 1;
     }
     else if (strlen(argv[1]) > 26)
     {
-        printf("Usage: ./substitution key\n");
+        std::cout << "Usage: ./substitution key" << std::endl;
         return 1;
     }
 }
-bool check_duli(string key)
+
+bool check_duli(std::string key)
 {
-    int length = strlen(key);
+    int length = key.length();
     for (int i = 0; i < length; i++)
     {
         key[i] = toupper(key[i]);
